ba6dd419cc84d8146001f8ccdb09de62eeff7a8c
soaringsoul <951683309@qq.com>
2023-02-06 22:23:17 +0800

    update to readthedocs

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'EasyPoi'
+copyright = '2020, 人文帮'
+author = '夜雨微寒'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables'
+]
+
+# extensions = [
+#     'sphinx.ext.duration',
+#     'sphinx.ext.doctest',
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.autosummary',
+#     'sphinx.ext.intersphinx',
+# ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to Easypoi's documentation!
+===================================
+
+**Easypoi**
+
+
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+   installer.md
